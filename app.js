9efcb58f2928b0d17acec276d455d49bd69812bf
Saurav Dev <devofficial998@gmail.com>
2023-07-27 15:22:02 +0530

    description added

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let filter=document.getElementById("filter")
 filter.addEventListener("keyup",xyz)
-
+let submit=document.getElementById("addForm")
+submit.addEventListener("submit",addItems)
 
 function xyz(e){
     let text=e.target.value.toLowerCase()
@@ -17,3 +18,29 @@ function xyz(e){
         }
     }
 }
+
+let x=document.createElement("input")
+x.setAttribute("id","descrp")
+x.setAttribute("type","text")
+x.setAttribute("placeholder","add description")
+x.setAttribute("class","form-control mr-2")
+
+let y=document.getElementById("addForm")
+let z=document.getElementById("sub")
+y.insertBefore(x,z)
+
+function addItems(e){
+    e.preventDefault()
+    let a=document.getElementById("item")
+    let b=document.getElementById("descrp")
+    let c=document.getElementById("items")
+    let newli=document.createElement("li")
+    newli.className="list-group-item"
+    newli.innerText=a.value+" & description : "+b.value
+    let btn=document.createElement("button")
+    btn.className="btn btn-danger btn-sm float-right delete"
+    btn.innerText="X"
+    newli.appendChild(btn)
+    c.appendChild(newli)
+}
+
